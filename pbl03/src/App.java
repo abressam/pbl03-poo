4400201df902eb3dad971623919a647fdb834fc8
abressam <amandabressam@gmail.com>
2023-03-24 23:24:47 -0300

    Create Mapa file

--- pbl03/src/App.java
+++ pbl03/src/App.java
@@ -1,10 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Suporte yuumi = new Suporte();
+        Mapa mapa = new Mapa();
+
+        mapa.setMapa("Summoner's Rift");
+        String nomeMapa = mapa.getMapa();
 
         yuumi.setNome("Yuumi");
         String nome = yuumi.getNome();
 
+        System.out.println("Bem-vindos a " + nomeMapa);
         System.out.println(nome);
     }
 }
---
+++ pbl03/src/Mapa.java
@@ -0,0 +1,11 @@
+public class Mapa {
+    private String mapa;
+
+    public void setMapa(String mapa) {
+        this.mapa = mapa;
+    }
+
+    public String getMapa() {
+        return mapa;
+    }
+}
